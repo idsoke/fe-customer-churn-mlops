3517481cccb1fc60482d36e61e187c24f265c8b8
idris <idrisnotebook@gmail.com>
2026-09-25 15:51:41 +0700

    set url api backend

--- src/app/core/churn.ts
+++ src/app/core/churn.ts
@@ -1,18 +1,17 @@
 import { inject, Service } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import type { CustomerData, ModelInfo, PredictionResponse } from './churn.models';
-
-const API_BASE_URL = 'http://localhost:8000';
 
 @Service()
 export class Churn {
   private readonly http = inject(HttpClient);
 
   predict(data: CustomerData) {
-    return this.http.post<PredictionResponse>(`${API_BASE_URL}/predict`, data);
+    return this.http.post<PredictionResponse>(`${environment.apiBaseUrl}/predict`, data);
   }
 
   getModelInfo() {
-    return this.http.get<ModelInfo>(`${API_BASE_URL}/model/info`);
+    return this.http.get<ModelInfo>(`${environment.apiBaseUrl}/model/info`);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://customer-churn-mlops.onrender.com',
+};
